d09d8502c4791cc7c29cc86ccdcba6abd6840656
Jackie Greenbaum <jgreenbaum@arbor.net>
2024-08-19 18:55:35 -0400

    update astro config to serve github pages site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://jackie-greenbaum.github.io',
+  base: 'src',
   integrations: [mdx(), sitemap(), tailwind()]
 });
